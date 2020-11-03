2beb2f8c8f3f89175b40d2db6167cac94eb06392
Dmitry Lavrik <admin@dmitrylavrik.ru>
2020-11-04 01:47:43 +0300

    images webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,47 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const webp = require('gulp-webp');
 const gcmq = require('gulp-group-css-media-queries');
 const critical = require('critical');
 
+let crPages = ['index', 'gallery'];
 let crList = {
 	'.btn': ['display', 'font-size', 'height', 'line-height', 'padding', 'text-align', 'border'],
 }
 
 function criticalCSS(done){
-	return critical.generate({
-		base: './dist/',
-		src: 'index.html',
-		css: [ 'css/main.css' ],
-		target: {
-			css: 'css/critical.css',
-			uncritical: 'css/async.css'
-		},
-		width: 1280,
-		height: 480,
-		include: [
-			'.footer'
-		],
-		ignore: {
-			rule: [
-				/hljs-/
+	crPages.forEach(async page => {
+		await critical.generate({
+			base: './dist/',
+			src: `${page}.html`,
+			css: [ 'css/main.css' ],
+			target: {
+				css: `css/${page}-critical.css`,
+				//uncritical: `css/${page}-async.css`
+			},
+			width: 1280,
+			height: 480,
+			include: [
+				'.footer'
 			],
-			decl(node, value){
-				let { selector } = node.parent; 
-						
-				if(!(selector in crList)){
-					return false;
+			ignore: {
+				rule: [
+					/hljs-/
+				],
+				decl(node, value){
+					let { selector } = node.parent; 
+							
+					if(!(selector in crList)){
+						return false;
+					}
+					
+					return !crList[selector].includes(node.prop);
 				}
-				
-				return !crList[selector].includes(node.prop);
 			}
-		}
+		});
 	});
-
-	//done();
+	
+	done();
 }
 
 function html(){
@@ -52,9 +56,22 @@ function css(){
 				.pipe(gulp.dest('./dist/css'));
 }
 
+function images(){
+	return gulp.src('./src/img/*')
+				.pipe(gulp.dest('./dist/img'));
+}
+
+function imagesWebp(){
+	return gulp.src('./src/img/*')
+				.pipe(webp())
+				.pipe(gulp.dest('./dist/img'));
+}
+
 gulp.task('html', html);
 gulp.task('css', css);
 gulp.task('critical', criticalCSS);
+gulp.task('images', images);
+gulp.task('webp', imagesWebp);
 
 gulp.task('build', 
 	gulp.series(
